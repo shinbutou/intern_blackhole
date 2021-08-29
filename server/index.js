f7f6bad13ba6e624ea0964640b21c643c092b56a
Ian <chenghsi.yang@gmail.com>
2021-08-29 23:14:27 +0800

    finishing the day

--- server/index.js
+++ server/index.js
@@ -3,3 +3,15 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
+
+//Middleware*
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = require('./routes/api/posts');
+
+app.use('/api/posts', posts);
+
+const port = 5000; // Heroku: || process.env.PORT
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
