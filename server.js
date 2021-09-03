246dc18aae0c591026a3d5e09ed91583db565309
Ian <chenghsi.yang@gmail.com>
2021-09-03 21:49:28 +0800

    finishing the day

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    console.log('hello world');
+    res.sendStatus(500);
+    res.send('response');
+});
+
 app.listen(8000);
